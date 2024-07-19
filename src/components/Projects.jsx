0ae16b8aa8189a55f3ce10e0a4df8ce5052627d4
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2024-07-19 12:28:18 +0100

    add: second project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -38,7 +38,7 @@ const Projects = () => {
       <h1>Selected Projects</h1>
       <div className="flex flex-col gap-20 mt-10">
 
-        <div className="flex gap-[41px]">
+        <div className="flex gap-[41px]" ref={textRef}>
           <div className="grid md:flex-[.7] flex-[.6] grow">
             <div className="mb-[25px] bg-[#FF6B35] py-[2.14px] px-[9.86px] inline-block self-end w-fit justify-self-start">Sentinel</div>
             <p className="md:text-lg md:text-left text-xs">Do you need an automated system specially built for your business processes? Say goodbye to the mundane and hello to efficiency and Let the magic of automation transform your business. Our meticulous team has got you covered. </p>
@@ -52,7 +52,12 @@ const Projects = () => {
         </div>
 
         <div className="flex gap-[41px] items-center">
-          <img src={cvApp} alt="strategy 2" className="md:flex-[.3] flex-[.4] w-[100px] md:w-[400px]" />
+          <div className="relative grow overflow-hidden w-[150px] md:max-w-[400px] right-16">
+              <div className="relative" style={{ paddingTop: '66.67%' }}>
+                <img src={cvApp} alt="strategy" className={`absolute top-0 left-0 right-0 bottom-0 w-full h-full transition-transform duration-500 ease-in-out transform ${tiltFrame ? "rotate-[5deg]" : ""} origin-top-left`} />
+              </div>
+            </div>
+          {/* <img src={cvApp} alt="strategy 2" className="md:flex-[.3] flex-[.4] w-[100px] md:w-[400px]" /> */}
           <div className="md:flex-[.7] flex-[.6] grid">
             <div className="mb-[25px] bg-[#FF6B35] py-[2.14px] px-[9.86px] inline-block self-end w-fit justify-self-start">Early Access to Our Forthcoming Products</div>
             <p className="md:text-lg md:text-left text-xs">Be among the first to experience our cutting-edge automation products before official launch. Gain early access to innovative features by opting in to our exclusive pre-release program, collaborating directly with Tecohol community of automation enthusiasts.</p>
