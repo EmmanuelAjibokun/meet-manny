1fd1ddb7a7da70ac3358c02e4ec13e7a71af474f
manny300 <96999656+EmmanuelAjibokun@users.noreply.github.com>
2024-08-01 22:22:40 +0100

    fix: resolve all problems"

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import multitasker from "../assets/multitasker.png"
 import { Projects } from "../components"
